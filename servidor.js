8d1dc94c61aa9f0527a83ddf7266198dd1869634
samsam-Dotchot <158769868+samsam-Dotchot@users.noreply.github.com>
2026-08-26 21:00:58 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -124,7 +124,7 @@ app.get('/api/personas', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
